f11242c4eb6c4849b63a634f46cf570f8a789ecf
Waseem <muhammadwaseem22095@gmail.com>
2024-10-06 17:46:15 +0500

    navbar

---
+++ src/Comonents/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className='main sm'>
+            <img className='image sm' src="/logoname.png" alt="logo" />
+      </div>
+    </div>
+  )
+}
